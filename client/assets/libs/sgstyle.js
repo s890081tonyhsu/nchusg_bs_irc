a24260ffbc5acc79404ec3ac7ce12c46d5b7ab1f
OtomeSound <s890081tonyhsu@gmail.com>
2013-12-05 17:52:55 +0800

    Add in nav into IRC page.
    1.	set some css to move a space to contain the nav.
    2.	Put in load.js to check if the nav_needed is exist or not.
    3.	Set sgstyle.js to make nav show after the css and js is settled up.
    4.	Put the nav source code into tmp file to let nodejs generate an index.

--- client/assets/libs/sgstyle.js
+++ client/assets/libs/sgstyle.js
@@ -1,5 +1,47 @@
 function toggleIntroduction(){
-	$('#description').slideToggle();
-	$('#introduction').slideToggle();
-	$('.server_select.initial').css("height","100%");
+	$('.server_select.initial').css("height","80%");
+	$('#description').slideToggle().queue(function(){
+		$('#introduction').slideToggle();
+	},100);
 }
+
+//$(document).ready(function(){
+//	if($('#kiwi').exists()){
+//		$('#aes_navbar').slideToggle();
+//	}
+//});
+
+(function ($) {
+
+/**
+* @function
+* @property {object} jQuery plugin which runs handler function once specified element is inserted into the DOM
+* @param {function} handler A function to execute at the time when the element is inserted
+* @param {bool} shouldRunHandlerOnce Optional: if true, handler is unbound after its first invocation
+* @example $(selector).waitUntilExists(function);
+*/
+
+$.fn.waitUntilExists    = function (handler, shouldRunHandlerOnce, isChild) {
+    var found       = 'found';
+    var $this       = $(this.selector);
+    var $elements   = $this.not(function () { return $(this).data(found); }).each(handler).data(found, true);
+
+    if (!isChild)
+    {
+        (window.waitUntilExists_Intervals = window.waitUntilExists_Intervals || {})[this.selector] =
+            window.setInterval(function () { $this.waitUntilExists(handler, shouldRunHandlerOnce, true); }, 500)
+        ;
+    }
+    else if (shouldRunHandlerOnce && $elements.length)
+    {
+        window.clearInterval(window.waitUntilExists_Intervals[this.selector]);
+    }
+
+    return $this;
+}
+
+}(jQuery));
+
+$('#kiwi').waitUntilExists(function(){
+	$('#aes_navbar').slideToggle("slow");
+});
